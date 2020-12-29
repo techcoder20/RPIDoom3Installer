ec46cf3b398ac332f327d7fbd12c198c915e8b10
RPICoder <68472305+techcoder20@users.noreply.github.com>
2020-12-28 20:41:05 -0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf ~/RPIDoom3Installer ~/Desktop/Doom3Demo.desktop ~/.local/share/applications/Doom3Demo.desktop
+sudo rm -rf ~/RPIDoom3Installer ~/Desktop/Doom3Demo.desktop ~/.local/share/applications/Doom3Demo.desktop
